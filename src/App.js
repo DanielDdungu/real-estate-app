3c376d630e98ca1ce9da4b34caf4898cd244344d
DanielDdungu <daddungu@outlook.com>
2022-02-02 13:58:01 +0100

    Signin

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Navbar from "./components/Navbar";
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Explore />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
-          <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-        <Navbar />
-      </Router>
-      {/** NAVIGATION BAR */}
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Explore />} />
+                    <Route
+                        path="/forgotpassword"
+                        element={<ForgotPassword />}
+                    />
+                    <Route path="/offers" element={<Offers />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                </Routes>
+                <Navbar />
+            </Router>
+            {/** NAVIGATION BAR */}
+        </>
+    );
 }
 
 export default App;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,67 +2,95 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 function SignIn() {
-  const [showPassword, setShowPassword] = useState(false);
-  const [formData, setFormData] = useState({ email: "", password: "" });
-  const { email, password } = formData;
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({ email: "", password: "" });
+    const { email, password } = formData;
 
-  const navigate = useNavigate();
-  const onChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  };
+    const navigate = useNavigate();
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+        }));
+    };
+    //Fuction for On submit form
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const auth = getAuth();
 
-  return (
-    <>
-      <div className="pageContainer">
-        <header>
-          <p className="pageHeader">Welcome Back</p>
-        </header>
-        <form>
-          <input
-            type="email"
-            className="emailInput"
-            placeholder="Email"
-            id="email"
-            value={email}
-            onChange={onChange}
-          />
-          <div className="passwordInputDiv">
-            <input
-              type={showPassword ? "text" : "password"}
-              className="passwordInput"
-              placeholder="Password"
-              id="password"
-              value={password}
-              onChange={onChange}
-            />
+            //Returns apromise
+            const userCredential = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
 
-            <img
-              src={visibilityIcon}
-              alt="show password"
-              className="showPassword"
-              onclick={() => setShowPassword((prevState) => !prevState)}
-            />
-          </div>
-          <Link to="/forgot-password" className="forgotPasswordLink">
-            Forgot Password
-          </Link>
-          <div className="signInBar">
-            <p className="signInText">Sign In</p>
-          </div>
-          <button className="signInButton">
-            <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
-          </button>
-        </form>
-        {/**GOOGLE AUNTHETICATION */}
-        <Link to="/sign-up" className="registerLink">
-          Sign Up Instead
-        </Link>
-      </div>
-    </>
-  );
+            //CHECK USER CREDENTIALS
+            if (userCredential.user) {
+                navigate("/");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+            <div className="pageContainer">
+                <header>
+                    <p className="pageHeader">Welcome Back</p>
+                </header>
+                <form onSubmit={onSubmit}>
+                    <input
+                        type="email"
+                        className="emailInput"
+                        placeholder="Email"
+                        id="email"
+                        value={email}
+                        onChange={onChange}
+                    />
+                    <div className="passwordInputDiv">
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            className="passwordInput"
+                            placeholder="Password"
+                            id="password"
+                            value={password}
+                            onChange={onChange}
+                        />
+
+                        <img
+                            src={visibilityIcon}
+                            alt="show password"
+                            className="showPassword"
+                            onClick={() =>
+                                setShowPassword((prevState) => !prevState)
+                            }
+                        />
+                    </div>
+                    <Link to="/forgot-password" className="forgotPasswordLink">
+                        Forgot Password
+                    </Link>
+                    <div className="signUpBar">
+                        <p className="signUpText">Sign In</p>
+                    </div>
+                    <button className="signUpButton">
+                        <ArrowRightIcon
+                            fill="#ffffff"
+                            width="34px"
+                            height="34px"
+                        />
+                    </button>
+                </form>
+                {/**GOOGLE AUNTHETICATION */}
+                <Link to="/signup" className="registerLink">
+                    Sign In Instead
+                </Link>
+            </div>
+        </>
+    );
 }
 export default SignIn;
